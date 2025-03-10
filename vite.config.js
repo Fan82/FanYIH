57a0b9ba167cd45340aca2762ecb86fea279d33e
Fan82 <93040903+Fan82@users.noreply.github.com>
2025-03-10 18:16:56 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path"; // 確保引入 path 模組
+import Prerender from "vite-plugin-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Prerender({
+      routes: ["/", "/about", "/contact"], // 預渲染的路由
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // 設定 `@` 代表 `src` 目錄
