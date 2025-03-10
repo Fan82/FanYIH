2daf8c1fd0948888d94223a35c188829f0f3db7c
Fan82 <93040903+Fan82@users.noreply.github.com>
2025-03-10 19:02:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path"; // 確保引入 path 模組
-import Prerender from "vite-plugin-prerender";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Prerender({
-      routes: ["/", "/about", "/contact"], // 預渲染的路由
-      staticDir: path.resolve(__dirname, "dist"), // 指定靜態文件目錄
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // 設定 `@` 代表 `src` 目錄
