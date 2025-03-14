d2b595f6352800b6760664399013b468e051b1b9
Fan82 <93040903+Fan82@users.noreply.github.com>
2025-03-14 13:06:26 +0800

    up

--- src/components/UserFlow.vue
+++ src/components/UserFlow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid">
+  <div>
     <img class="project-img" :src="project.flowImage" :alt="project.name" :class="{ 'slide-scrolled': isScrolled }" />
   </div>
 </template>
@@ -15,3 +15,20 @@ export default {
 
 };
 </script>
+
+<style lang="sass" scoped>
+  @use '@/assets/styles/_mixins.sass' as *
+  @use '@/assets/styles/_variables.sass' as *
+
+  .project-img
+      background-color: transparent
+      box-shadow: none
+  @media (max-width: 500px)
+    div
+      overflow: hidden
+      overflow-x: auto 
+      height: 100vh
+    .project-img
+      transform: scale(2.5)
+      transform-origin: left top
+</style>
