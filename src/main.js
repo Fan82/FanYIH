c6063f644413ef361f7e012a6a4f8b4e449e38a1
Fan82 <93040903+Fan82@users.noreply.github.com>
2025-03-14 15:08:31 +0800

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-// src/main.js
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router"; // 引入 router 配置
-import "@/assets/styles/styles.sass"; // 引入全局樣式
-
-createApp(App)
-  .use(router) // 使用 router
-  .mount("#app");
